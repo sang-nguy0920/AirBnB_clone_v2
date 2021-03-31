80c923480e3ccfcf40c29e0f80afb70d0f849885
sang-nguy0920 <2321@holbertonschool.com>
2021-03-31 23:57:33 +0000

    testing city

--- models/city.py
+++ models/city.py
@@ -14,12 +14,8 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        state = relationship("State", backref="City")
-        # places = relationship("Place", cascade="all, delete-orphan")
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    state = relationship("State", backref="City")
+    # places = relationship("Place", cascade="all, delete-orphan")
 
-    else:
-        name = ""
-        state_id = ""
