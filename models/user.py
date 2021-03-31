c1c65fceb919e830df39a0aae790741f4f6280fa
sang-nguy0920 <2321@holbertonschool.com>
2021-03-31 23:23:45 +0000

    Added code to users.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 
-
+import models
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
