5d34e89a0103e43ebc69e7cc64db3fb66ffde130
sang-nguy0920 <2321@holbertonschool.com>
2021-03-31 23:59:06 +0000

    update to state

--- models/state.py
+++ models/state.py
@@ -14,17 +14,5 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete-orphan")
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            instance_state = []
-            for key, obj in models.storage.all().items():
-                if obj.__class__.__name__ == 'City':
-                    if obj.state_id == self.id:
-                        instance_state.append(obj)
-            return instance_state
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete-orphan", backref='State')
