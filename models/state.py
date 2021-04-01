202728d9934ac822d2cdd4e3f0712328c8ed141c
sang-nguy0920 <2321@holbertonschool.com>
2021-04-01 00:21:43 +0000

    updated state

--- models/state.py
+++ models/state.py
@@ -15,4 +15,4 @@ class State(BaseModel, Base):
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan", backref='State')
+    cities = relationship("City", cascade="delete", backref='State')
