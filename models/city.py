66da9ade4164a63b3f21db79b3e74a6ceb72b960
sang-nguy0920 <2321@holbertonschool.com>
2021-04-01 02:34:42 +0000

    updated city.py

--- models/city.py
+++ models/city.py
@@ -14,8 +14,7 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
 
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    state = relationship("State", backref="City")
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", backref="cities",
                           cascade="delete")
