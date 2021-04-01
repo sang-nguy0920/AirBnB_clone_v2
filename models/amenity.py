c2354b923fed21ab46b1257c90e6e8a3cac27129
sang-nguy0920 <2321@holbertonschool.com>
2021-04-01 03:15:39 +0000

    reverted for 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
-
-import models
-from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
-from sqlalchemy.orm import relationship
-from models.place import place_amenity
-from os import getenv
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-
-    """ A place to stay """
-    __tablename__ = 'amenities'
-
-    name = Column(String(128), nullable=False)
+    name = ""
